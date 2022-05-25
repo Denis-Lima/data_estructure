e5b16dd0332621877a222173c64763113d006b46
Denis Lima <denislima99@gmail.com>
2022-05-24 23:01:41 -0300

    Simplificando linked_list

--- python/structure/linked_list.py
+++ python/structure/linked_list.py
@@ -1,127 +1,136 @@
-from typing import Any, Union
+from typing import Any
+
 
 class Node():
-    def __init__(self, data: Any) -> None:
+    def __init__(self, data: Any, next_: 'Node') -> None:
         self.data = data
-        self.next = None
+        self.next = next_
 
-    def __str__(self) -> str:
-        next = str(self.next) if not self.next else str(self.next.data)
-        return str(self.data) + ' -> ' + next
 
-    def __repr__(self) -> str:
-        return str(self)
 class LinkedList():
     def __init__(self) -> None:
         self.head = None
-        self.size = 0
+        self.__size = 0
 
     def __len__(self):
-        return self.size
+        return self.__size
 
     def __str__(self) -> str:
         string = []
         node: Node = self.head
-        for i in range(self.size):
+        for _ in range(self.__size):
             string.append(str(node.data))
             node = node.next
-        return '[' + ' -> '.join(string) + ']'
+        return '-->'.join(string)
 
     def __repr__(self) -> str:
         return str(self)
 
-    def get_node(self, index) -> Union[Node, None]:
-        if self.empty() or index >= len(self) or index < 0:
-            raise IndexError('Index is out of range')
+    def __iter__(self):
+        return self.items()
 
-        node = self.head
-        for i in range(index):
-            if not node.next:
-                break
-            node = node.next
-        return node
+    def insert_first(self, data: Any):
+        self.head = Node(data, self.head)
+        self.__size += 1
 
-    def insert(self, index: int, data: Any) -> None:
-        if index == 0:
-            self.insert_first(data)
-            return
-        elif index == self.size:
-            self.insert_last(data)
-            return
-
-        new_node = Node(data)
-        previous_node = self.get_node(index - 1)
-        new_node.next, previous_node.next = previous_node.next, new_node
-        self.size += 1
-
-    def insert_first(self, data: Any) -> None:
-        new_node = Node(data)
-        if not self.size:
-                self.head = new_node
-                self.size += 1
-                return
-        new_node.next, self.head = self.head, new_node
-        self.size += 1
-    
-    def insert_last(self, data: Any) -> None:
-        if self.empty():
-            self.head = Node(data)
-            self.size += 1
-            return
-        node = self.head
-        while True:
-            if not node.next:
-                break
-            node = node.next
-        node.next = Node(data)
-        self.size += 1
-
-    def get_first(self) -> Union[Any, None]:
-        first = self.head
-        if first:
-            return first.data
-        return first #None
-
-    def get_last(self) -> Union[Any, None]:
-        return self.get(self.size)
-
-    def get(self, index) -> Union[Any, None]:
-        node = self.get_node(index)
-        if node:
-            return node.data
-        return node
-    
-    def empty(self) -> bool:
-        return self.size == 0
-
-    def items(self) -> Node:
-        current = self.head
-        while current:
-            yield current
-            current = current.next
-    
-    def remove_first(self) -> Node:
-        if self.empty():
-            raise IndexError('Index is out of range')
-        removed = self.head
+    def remove_first(self) -> Any:
+        if self.head is None:
+            raise IndexError(f'Cannot remove in list with size {self.__size}')
+        node: Node = self.head
         self.head = self.head.next
-        removed.next = None
-        self.size -= 1
-        return removed
-    
-    def remove_last(self) -> Node:
-        return self.remove(self.size - 1)
-    
-    def remove(self, index: int) -> Node:
-        if index >= self.size:
-            raise IndexError('Index is out of range')
-        
+        self.__size -= 1
+        return node.data
+
+    def insert_at(self, index: int, data: Any):
+        if index == 0:
+            return self.insert_first(data)
+        if index < 0 or index >= len(self):
+            raise IndexError(f'Index {index} is out of range')
+
+        current_node = self.head
+        current_pos = 0
+        while current_pos < index - 1:
+            current_node = current_node.next
+            current_pos += 1
+        current_node.next = Node(data, current_node.next)
+        self.__size += 1
+
+    def remove_at(self, index: int) -> Any:
         if index == 0:
             return self.remove_first()
-        
-        preview_node = self.get_node(index - 1)
-        removed = preview_node.next
-        if removed:
-            preview_node.next, removed.next = removed.next, None
-        self.size -= 1
-        return removed
+        if index < 0 or index >= len(self):
+            raise IndexError(f'Index {index} is out of range')
+
+        current_node = self.head
+        current_pos = 0
+        while current_pos < index - 1:
+            current_node = current_node.next
+            current_pos += 1
+        node = current_node.next
+        current_node.next = current_node.next.next
+        self.__size -= 1
+        return node.data
+
+    def get_first(self) -> Any:
+        first = self.head
+        if first is not None:
+            return first.data
+        return first  # None
+
+    def get_last(self) -> Any:
+        return self.get(len(self) - 1)
+
+    def get(self, index: int) -> Any:
+        if index == 0:
+            return self.get_first()
+        if index < 0 or index >= len(self):
+            raise IndexError(f'Index {index} is out of range')
+
+        current_node = self.head
+        current_pos = 0
+        while current_pos != index:
+            current_node = current_node.next
+            current_pos += 1
+        return current_node.data
+
+    def empty(self) -> bool:
+        return self.__size == 0
+
+    def items(self) -> Any:
+        current = self.head
+        while current is not None:
+            yield current.data
+            current = current.next
+
+
+if __name__ == '__main__':
+    lista = LinkedList()
+    print('Is empty?', lista.empty())
+    print('Size:', len(lista))
+    lista.insert_at(0, 1)
+    lista.insert_at(0, 999)
+    print(lista)
+    lista.insert_first(50)
+    lista.insert_at(2, 600)
+    print(lista)
+
+    print('Removed(last):', lista.remove_at(len(lista) - 1))
+    print('Removed(first):', lista.remove_first())
+    print(lista)
+
+    lista.insert_at(1, 40)
+    lista.insert_at(2, 7)
+    print('First:', lista.get_first())
+    print('Last:', lista.get_last())
+    print('At 2:', lista.get(2))
+    print(lista)
+    print('Is empty?', lista.empty())
+    print('Size:', len(lista))
+
+    print('Iterating over items: ', end='')
+    for item in lista.items():
+        print(item, end=' ')
+
+    print('\nIterating over items2: ', end='')
+    for item in lista:
+        print(item, end=' ')
